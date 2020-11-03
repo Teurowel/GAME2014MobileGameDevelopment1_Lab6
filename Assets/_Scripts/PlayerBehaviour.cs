8d061086dad6d230ff557f1782442a061f1eac0a
Teurowel <teurowel@gmail.com>
2020-11-04 01:48:47 +0900

    Added player controller

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -8,13 +8,19 @@ public class PlayerBehaviour : MonoBehaviour
     public float joystickSensitivity;
     public float horizontalForce;
     public float jumpForce;
-    public float maximumVelocityX;
-    public Rigidbody2D rigidBody2D;
+    public bool isGrounded;
+
+    
+    private Rigidbody2D rigidBody2D;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,16 +31,43 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void _Move()
     {
-        if (joystick.Horizontal > joystickSensitivity)
+        if(isGrounded)
         {
-            //move to right
-            rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+            if (joystick.Horizontal > joystickSensitivity)
+            {
+                //move to right
+                rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = false;
+                animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Horizontal < -joystickSensitivity)
+            {
+                //move to left
+                rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = true;
+                animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Vertical > joystickSensitivity)
+            {
+                //Jump
+                rigidBody2D.AddForce(Vector2.up * jumpForce * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetInteger("AnimState", 0);
+            }
         }
 
-        if (joystick.Horizontal < -joystickSensitivity)
-        {
-            //move to left
-            rigidBody2D.AddForce(Vector2.left* horizontalForce * Time.deltaTime);
-        }
+      
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
